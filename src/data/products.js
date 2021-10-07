68f462f5dfbea7f6eff839dee1ab225c5dc14df7
paukasha <odinpozitivchik@mail.ru>
2021-10-07 21:37:39 +1000

    initial commit

---
+++ src/data/products.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    title: 'Радионяня Motorolla MBP16',
+    price: 3690,
+    image: '/img/radio.jpg'
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg'
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: '/img/phone.jpg'
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 3690,
+    image: '/img/board.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14960,
+    image: '/img/phone-2.jpg'
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    image: '/img/phone-3.jpg'
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    image: '/img/pic-square-2.jpg'
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34900,
+    image: '/img/scooter.jpg'
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    image: '/img/wheels.jpg'
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    image: '/img/headphones.jpg'
+  },
+  {
+    title: 'Лего',
+    price: 500,
+    image: '/img/lego1.jpg'
+  },
+  {
+    title: 'Барби',
+    price: 100,
+    image: '/img/barbie2.jpg'
+  },
+  {
+    title: 'Хомяк',
+    price: 5000,
+    image: '/img/homyak.jpg'
+  },
+  {
+    title: 'Конструктор',
+    price: 5000,
+    image: '/img/construct3.jpg'
+  },
+]
---
+++ src/message.js
@@ -0,0 +1,2 @@
+export const message1 = 'Hello';
+export const message2 = 'Vue';
