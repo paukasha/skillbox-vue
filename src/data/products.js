13fa51e49a1fbc258490479ff39881a3a161af69
paukasha <odinpozitivchik@mail.ru>
2021-10-09 11:35:00 +1000

    add images

--- src/data/products.js
+++ src/data/products.js
@@ -1,53 +1,33 @@
 export default [
   {
-    title: 'Радионяня Motorolla MBP16',
-    price: 3690,
-    image: '/img/radio.jpg'
+    title: 'Стиральный порошок',
+    price: 369,
+    image: '/img/washing-powder.jpg'
   },
   {
-    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
-    price: 5660,
+    title: 'Зубная щётка',
+    price: 56,
     image: '/img/toothbrush.jpg'
   },
   {
-    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
-    price: 21790,
-    image: '/img/phone.jpg'
+    title: 'Телефон',
+    price: 2179,
+    image: '/img/telephone.jpg'
   },
   {
-    title: 'Электроскейт Razor Cruiser',
-    price: 3690,
-    image: '/img/board.jpg',
+    title: 'Обогреватель',
+    price: 390,
+    image: '/img/hotcreator.jpg',
   },
   {
-    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
-    price: 14960,
-    image: '/img/phone-2.jpg'
+    title: 'Кружка',
+    price: 149,
+    image: '/img/cap.jpg'
   },
   {
-    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    title: 'Кофе',
     price: 8960,
-    image: '/img/phone-3.jpg'
-  },
-  {
-    title: 'Электрический дрифт-карт Razor Crazy Cart',
-    price: 39900,
-    image: '/img/pic-square-2.jpg'
-  },
-  {
-    title: 'Гироскутер Razor Hovertrax 2.0',
-    price: 34900,
-    image: '/img/scooter.jpg'
-  },
-  {
-    title: 'Роллерсёрф Razor RipStik Air Pro',
-    price: 6690,
-    image: '/img/wheels.jpg'
-  },
-  {
-    title: 'Наушники AirPods с беспроводным зарядным футляром',
-    price: 16560,
-    image: '/img/headphones.jpg'
+    image: '/img/coffee.jpg'
   },
   {
     title: 'Лего',
