825796a8f2f25175811e9295968d8d4c1f7c15e2
paukasha <odinpozitivchik@mail.ru>
2021-11-20 09:02:11 +1000

    Модуль 6 - недоделано. уроки 6.3 - 6.4

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -8,7 +8,7 @@
           </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <router-link :to="{name: 'main'}" class="breadcrumbs__link"  >
+          <router-link :to="{name: 'main'}" class="breadcrumbs__link">
             {{ category.title }}
           </router-link>
         </li>
@@ -28,22 +28,26 @@
         <ul class="pics__list">
           <li class="pics__item">
             <a href="" class="pics__link pics__link--current">
-              <img width="98" height="98" src="img/phone-square-1.jpg" srcset="img/phone-square-1@2x.jpg 2x" alt="Название товара">
+              <img width="98" height="98" src="img/phone-square-1.jpg"
+                   srcset="img/phone-square-1@2x.jpg 2x" alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a href="" class="pics__link">
-              <img width="98" height="98" src="img/phone-square-2.jpg" srcset="img/phone-square-2@2x.jpg 2x" alt="Название товара">
+              <img width="98" height="98" src="img/phone-square-2.jpg"
+                   srcset="img/phone-square-2@2x.jpg 2x" alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a href="" class="pics__link">
-              <img width="98" height="98" src="img/phone-square-3.jpg" srcset="img/phone-square-3@2x.jpg 2x" alt="Название товара">
+              <img width="98" height="98" src="img/phone-square-3.jpg"
+                   srcset="img/phone-square-3@2x.jpg 2x" alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a class="pics__link" href="#">
-              <img width="98" height="98" src="img/phone-square-4.jpg" srcset="img/phone-square-4@2x.jpg 2x" alt="Название товара">
+              <img width="98" height="98" src="img/phone-square-4.jpg"
+                   srcset="img/phone-square-4@2x.jpg 2x" alt="Название товара">
             </a>
           </li>
         </ul>
@@ -55,7 +59,7 @@
           {{ product.title }}
         </h2>
         <div class="item__form">
-          <form class="form" action="#" method="POST">
+          <form class="form" action="#" method="POST" @submit.prevent="addToCart">
             <b class="item__price">
               {{ product.price | numberFormat }} ₽
             </b>
@@ -65,21 +69,24 @@
               <ul class="colors">
                 <li class="colors__item">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="blue" checked="">
+                    <input class="colors__radio sr-only" type="radio" name="color-item" value="blue"
+                           checked="">
                     <span class="colors__value" style="background-color: #73B6EA;">
                     </span>
                   </label>
                 </li>
                 <li class="colors__item">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="yellow">
+                    <input class="colors__radio sr-only" type="radio" name="color-item"
+                           value="yellow">
                     <span class="colors__value" style="background-color: #FFBE15;">
                     </span>
                   </label>
                 </li>
                 <li class="colors__item">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="gray">
+                    <input class="colors__radio sr-only" type="radio" name="color-item"
+                           value="gray">
                     <span class="colors__value" style="background-color: #939393;">
                   </span></label>
                 </li>
@@ -108,7 +115,8 @@
                 </li>
                 <li class="sizes__item">
                   <label class="sizes__label">
-                    <input class="sizes__radio sr-only" type="radio" name="sizes-item" value="128" checked="">
+                    <input class="sizes__radio sr-only" type="radio" name="sizes-item" value="128"
+                           checked="">
                     <span class="sizes__value">
                       128gb
                     </span>
@@ -118,21 +126,8 @@
             </fieldset>
 
             <div class="item__row">
-              <div class="form__counter">
-                <button type="button" aria-label="Убрать один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-minus"></use>
-                  </svg>
-                </button>
 
-                <input type="text" value="1" name="count">
-
-                <button type="button" aria-label="Добавить один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-plus"></use>
-                  </svg>
-                </button>
-              </div>
+              <ItemQuantity :amount="amount"/>
 
               <button class="button button--primery" type="submit">
                 В корзину
@@ -181,13 +176,27 @@
           <h3>Что это?</h3>
 
           <p>
-            Wahoo ELEMNT BOLT GPS – это велокомпьютер, который позволяет оптимизировать свои велотренировки, сделав их максимально эффективными. Wahoo ELEMNT BOLT GPS синхронизируется с датчиками по ANT+, объединяя полученную с них информацию. Данные отображаются на дисплее, а также сохраняются на смартфоне. При этом на мобильное устройство можно установить как фирменное приложение, так и различные приложения сторонних разработчиков. Велокомпьютер точно отслеживает местоположение, принимая сигнал с целого комплекса спутников. Эта информация позволяет смотреть уже преодоленные маршруты и планировать новые велопрогулки.
+            Wahoo ELEMNT BOLT GPS – это велокомпьютер, который позволяет оптимизировать свои
+            велотренировки, сделав их максимально эффективными. Wahoo ELEMNT BOLT GPS
+            синхронизируется с датчиками по ANT+, объединяя полученную с них информацию. Данные
+            отображаются на дисплее, а также сохраняются на смартфоне. При этом на мобильное
+            устройство можно установить как фирменное приложение, так и различные приложения
+            сторонних разработчиков. Велокомпьютер точно отслеживает местоположение, принимая сигнал
+            с целого комплекса спутников. Эта информация позволяет смотреть уже преодоленные
+            маршруты и планировать новые велопрогулки.
           </p>
 
           <h3>Дизайн</h3>
 
           <p>
-            Велокомпьютер Wahoo ELEMNT BOLT очень компактный. Размеры устройства составляют всего 74,6 x 47,3 x 22,1 мм. что не превышает габариты смартфона. Корпус гаджета выполнен из черного пластика. На обращенной к пользователю стороне расположен дисплей диагональю 56 мм. На дисплей выводятся координаты и скорость, а также полученная со смартфона и синхронизированных датчиков информация: интенсивность, скорость вращения педалей, пульс и т.д. (датчики не входят в комплект поставки). Корпус велокомпьютера имеет степень защиты от влаги IPX7. Это означает, что устройство не боится пыли, а также выдерживает кратковременное (до 30 минут) погружение в воду на глубину не более 1 метра.
+            Велокомпьютер Wahoo ELEMNT BOLT очень компактный. Размеры устройства составляют всего
+            74,6 x 47,3 x 22,1 мм. что не превышает габариты смартфона. Корпус гаджета выполнен из
+            черного пластика. На обращенной к пользователю стороне расположен дисплей диагональю 56
+            мм. На дисплей выводятся координаты и скорость, а также полученная со смартфона и
+            синхронизированных датчиков информация: интенсивность, скорость вращения педалей, пульс
+            и т.д. (датчики не входят в комплект поставки). Корпус велокомпьютера имеет степень
+            защиты от влаги IPX7. Это означает, что устройство не боится пыли, а также выдерживает
+            кратковременное (до 30 минут) погружение в воду на глубину не более 1 метра.
           </p>
         </div>
       </div>
@@ -200,7 +209,18 @@ import products from '@/data/products'
 import categories from '@/data/categories'
 import goToPage from '@/helpers/goToPage'
 import numberFormat from '@/helpers/numberFormat'
+import ItemQuantity from '@/components/ItemQuantity'
+
 export default {
+  data() {
+    return {
+      amount: 1
+    }
+  },
+
+  components: {
+    ItemQuantity
+  },
   filters: {
     numberFormat
   },
@@ -209,11 +229,27 @@ export default {
       return products.find(product => product.id === +this.$route.params.id)
     },
     category() {
-      return categories.find(category => category.id === this.product.categoryId )
+      return categories.find(category => category.id === this.product.categoryId)
     }
   },
   methods: {
-    goToPage
+    goToPage,
+    addToCart() {
+      // так делать нельзя!!!
+      // this.$store.state.cartProducts.push({
+      //   productId: this.product.id,
+      //   amount: this.productAmount
+      // })
+
+      //смотри в store, там пушится объект
+      this.$store.commit(
+        "addProductToCart",
+        {
+          productId: this.product.id,
+          amount: this.amount
+        })
+
+    },
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,36 @@ import VueRouter from 'vue-router'
 import MainPage from '@/pages/MainPage'
 import notFound from '@/pages/NotFound'
 import ProductPage from '@/pages/ProductPage'
+import CartPage from '@/pages/CartPage'
+
 Vue.use(VueRouter)
 
 const routes = [
-    //порядок имеет значение
-    {
-        name: 'main',
-        component: MainPage,
-        path: '/'
-    },
-    {
-        name: 'product',
-        component: ProductPage,
-        path: '/product/:id'  //указываем динамический путь, после двоеточия указывает название динмического сегментаб например id
-    },
-    {
-        name: 'notFound',
-        component: notFound,
-        path: '*'
-    },
+  //порядок имеет значение
+  {
+    name: 'main',
+    component: MainPage,
+    path: '/'
+  },
+  {
+    name: 'product',
+    component: ProductPage,
+    path: '/product/:id'  //указываем динамический путь, после двоеточия указывает название динмического сегментаб например id
+  },
+  {
+    name: 'cart',
+    component: CartPage,
+    path: '/cart'
+  },
+  {
+    name: 'notFound',
+    component: notFound,
+    path: '*'
+  },
 ]
 
 const router = new VueRouter({
-    routes
+  routes
 })
 
 export default router
